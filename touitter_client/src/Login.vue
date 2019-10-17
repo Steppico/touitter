bb97455c9d771d5bff9e8eaeb146ea5898b9fd6d
Steppico <steppashr@gmail.com>
2019-10-17 16:33:23 +0900

    create login layout

---
+++ touitter_client/src/Login.vue
@@ -0,0 +1,79 @@
+<template>
+<div id="login">
+  <body>
+    <h1>tOUItter</h1>
+    <span v-if="signup">
+      <div class="login_panel">
+        <h3>Login</h3>
+        <input type="text" name="email" v-model="input.email" placeholder="email" />
+        <input type="password" name="password" v-model="input.password" />
+        <button type="button" v-on:click="login">Login</button>
+        <a>Not a member?</a>
+        <a class="signup_click" v-on:click="goToSignUp" v-on="hover">Sign up</a>
+      </div>
+    </span>
+    <span v-if="!signup">
+      <Signup v-bind:goToSignUp="goToSignUp" />
+    </span>
+  </body>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import Signup from "./components/Signup";
+
+export default {
+  name: "login",
+  data() {
+    return {
+      input: {
+        email: "",
+        password: ""
+      },
+      count: 0,
+      signup: true
+    };
+  },
+
+  components: {
+    Signup
+  },
+  methods: {
+    login: function() {
+      this.count++;
+    },
+    goToSignUp: function() {
+      this.signup = !this.signup;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#login {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.login_panel {
+  border: 1px solid black;
+  width: 400px;
+  text-align: center;
+  display: inline-block;
+}
+
+.signup_click:hover {
+  cursor: pointer;
+  color: blue;
+}
+
+h1 {
+  color: rgb(0, 100, 200);
+}
+</style>
+<style>
+body {
+  text-align: center;
+  display: inline-block;
+}
+</style>
---
+++ touitter_client/src/components/Signup.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="signup_box">
+    <a>Email</a>
+    <input
+      type="text"
+      name="signup-email"
+      v-model="input.signupemail"
+      placeholder="john.doe@example.com"
+    />
+    <a>Password</a>
+    <input type="password" name="signup-password" v-model="input.signUpPassword" />
+    <a>Confirm Password</a>
+    <input type="password" name="signup-confirm-password" v-model="input.confirmPassword" />
+    <button type="button" v-on:click="confirmation">Confirm</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Signup",
+  props: ["goToSignUp"],
+  data() {
+    return {
+      input: {
+        signupemail: "",
+        signUpPassword: "",
+        confirmPassword: ""
+      }
+    };
+  },
+  methods: {
+    confirmation: function() {
+      if (
+        this.input.signupemail !== "" &&
+        this.input.signUpPassword !== "" &&
+        this.input.confirmPassword !== "" &&
+        this.input.signUpPassword === this.input.confirmPassword
+      ) {
+        alert("Registration completed! Thank you");
+        this.goToSignUp();
+      } else if (this.input.signUpPassword !== this.input.confirmPassword) {
+        alert("Password inserted is wrong. Please type again.");
+        this.input.signUpPassword = "";
+        this.input.confirmPassword = "";
+      } else {
+        alert("Please complete the form");
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+a {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.signup_box {
+  display: flex;
+  align-items: center;
+}
+</style>
+
+
+ 
