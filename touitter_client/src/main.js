134096da09b8ea89a0abc01b2ff5dcbbee39ad93
Steppico <steppashr@gmail.com>
2019-10-17 19:19:10 +0900

    install apollo

--- touitter_client/src/main.js
+++ touitter_client/src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import Login from "./Login.vue";
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  apolloProvider: createProvider(),
   render: h => h(Login)
 }).$mount("#app");
